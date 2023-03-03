5cb871d9caa5dd1df34ae7bb778a875cb4481a83
valeraus <vlaus0@gmail.com>
2023-03-03 13:57:07 +0200

    додав додаткове завдання

---
+++ Essential/Lesson7/AddTask/Notebook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    /*
+        Створіть структуру з ім'ям – Notebook. Поля структури: модель, виробник, вартість. 
+        У структурі має бути реалізований конструктор для ініціалізації полів та метод для виведення вмісту полів на екран. 
+ */
+    internal struct Notebook
+    {
+        private string model;
+        private string vyrobnyk;
+        private int cost;
+
+        public Notebook (string model, string vyrobnyk, int cost)
+        {
+            this.model = model;
+            this.vyrobnyk = vyrobnyk;
+            this.cost = cost;
+        }
+        public Notebook(string model)
+            : this(model, "", 0)
+        {
+        }
+        public Notebook(int cost)
+            : this("", "", cost)
+        {
+        }
+        public string Model { get { return model; } }
+        public string Vyrobnyk { get {  return vyrobnyk; } }
+        public int Cost { get { return cost; } }
+
+        public void Show()
+        {
+            Console.WriteLine("Модель: {0}, Виробник:  {1}, Вартість: {2}$ ", model, vyrobnyk, cost);
+        }
+    }
+}
---
+++ Essential/Lesson7/AddTask/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTask
+{
+    /*
+    Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+    Створіть структуру з ім'ям – Notebook. Поля структури: модель, виробник, вартість. 
+    У структурі має бути реалізований конструктор для ініціалізації полів та метод для виведення вмісту полів на екран. 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Notebook notebook1 = new Notebook("X556U", "ASUS", 570);
+            notebook1.Show();
+
+            Notebook notebook2 = new Notebook("Inspirion");
+            notebook2.Show();
+
+            //Notebook notebook3 = new Notebook();
+            //notebook3.Show();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
