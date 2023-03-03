93e25a32b765a33c17f734b1fb24a690f156e580
valeraus <vlaus0@gmail.com>
2023-03-03 15:50:12 +0200

    додав завдання 3

---
+++ Essential/Lesson8/Task3/Accountant.cs
@@ -0,0 +1,28 @@
+﻿namespace Task3
+{
+    enum Post
+    {
+        // посади співробітників і кількість відпрацьованих годин за місяць
+        Manager = 195,
+        Developer = 200,
+        Accountant = 192,
+        Secretary = 192,
+        Cleaner = 192
+    }
+
+    class Accauntant
+    {
+        // метод який рахує давати співробітнику премію чи ні, якщо співробітник відпрацював 192 години на місяць і більше - премію давати
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Essential/Lesson8/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    /*
+    Використовуючи Visual Studio, створіть проект за шаблоном Console Application. 
+    Створіть перелік, який містить посади співробітників як імена констант. 
+    Надайте кожній константі значення, що задає кількість годин, які повинен відпрацювати співробітник протягом місяця. 
+    Створіть клас Accountant з методом bool AskForBonus (Post worker, int hours), що відображатиме давати співробітнику премію. 
+    Якщо співробітник відпрацював більше годин на місяць, то йому належить премія.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.Write("Введіть кільскість відпрацьованих годин: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            Accauntant a = new Accauntant();
+
+            if (a.AskForBonus(Post.Cleaner, hours))
+            {
+                Console.WriteLine("Дати премію");
+            }
+            else
+            {
+                Console.WriteLine("Не давати премію");
+            }
+
+            //Delay.
+            Console.ReadKey();
+        }
+    }
+}
